4b2ccbca417d815227726b8740c3d5e608111693
Vladislav Rubanov <uladzik@mail.ru>
2020-09-27 13:22:29 +0300

    add new task in arrays

--- by/epam/training/module02/array/task09/Module02Task09.java
+++ by/epam/training/module02/array/task09/Module02Task09.java
@@ -4,7 +4,24 @@ package by.epam.training.module02.array.task09;
 // найти наиболее часто встречающееся число.
 // Если таких чисел несколько, то определить наименьшее из них.
 
+import java.util.Arrays;
+
 public class Module02Task09 {
 
-    
-}
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{8, 12, 13, -3, 17, -9, 12, 22, 13};
+        int repeat = 1;
+        int num = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j]) {
+                    repeat++;
+                    num = numbers[i];
+                    System.out.println(numbers[i]);
+                }
+                }
+            }
+        }
+    }

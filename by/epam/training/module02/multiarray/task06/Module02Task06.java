d26795757adec94c508a4b9fea957a3ba061c7df
Vladislav Rubanov <uladzik@mail.ru>
2020-10-04 23:27:03 +0300

    add new task in multiarrays

--- by/epam/training/module02/multiarray/task06/Module02Task06.java
+++ by/epam/training/module02/multiarray/task06/Module02Task06.java
@@ -9,4 +9,29 @@ package by.epam.training.module02.multiarray.task06;
 //      1 1 1 ... 1 1 1
 
 public class Module02Task06 {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < n / 2 + 1; i++) {
+            for (int j = i; j < n - i; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            for (int j = n - i - 1; j < i + 1; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }

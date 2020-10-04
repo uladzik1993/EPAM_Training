bf969efd242869c1b47a7581bb70b313be8ed32a
Vladislav Rubanov <uladzik@mail.ru>
2020-10-04 23:28:23 +0300

    add new task in multiarrays

--- by/epam/training/module02/multiarray/task07/Module02Task07.java
+++ by/epam/training/module02/multiarray/task07/Module02Task07.java
@@ -3,7 +3,7 @@ package by.epam.training.module02.multiarray.task07;
 // Сформировать квадратную матрицу порядка N по правилу:
 // A [I, J] = sin(I(2) - J(2) / N)
 
-public class Module02Task08 {
+public class Module02Task07 {
 
     public static void main(String[] args) {
 

58c54ea71b725997af874605a203e6acf3a6875e
Vladislav Rubanov <uladzik@mail.ru>
2020-10-05 00:44:20 +0300

    add new task in multiarrays

--- by/epam/training/module02/multiarray/task08/Module02Task08.java
+++ by/epam/training/module02/multiarray/task08/Module02Task08.java
@@ -4,5 +4,56 @@ package by.epam.training.module02.multiarray.task08;
 // на соответствующие им позиции другого, а его элементы второго переместить в первый. Номера столбцов вводит
 // пользователь с клавиатуры.
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Module02Task08 {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        int N = 6;
+        int[][] matrix = new int[N][N];
+        Random rand = new Random();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = rand.nextInt(10);
+            }
+        }
+        System.out.println("Получилапсь матрица: ");
+        System.out.println();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Укажите номера столбцов которые нужно поменять местами");
+
+        System.out.println("Первый: ");
+        int replace1 = scan.nextInt();
+
+        System.out.println("Второй: ");
+        int replace2 = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+                int tmp = matrix[i][replace1 - 1];
+                matrix[i][replace1 - 1] = matrix[i][replace2 - 1];
+                matrix[i][replace2 - 1] = tmp;
+            }
+
+        System.out.println("Вот что получилось: ");
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }

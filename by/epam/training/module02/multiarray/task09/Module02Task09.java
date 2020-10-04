2d42ca3fad2ae18315aa38971f1cc427ad51c778
Vladislav Rubanov <uladzik@mail.ru>
2020-10-05 00:52:48 +0300

    add new task in multiarrays

---
+++ by/epam/training/module02/multiarray/task09/Module02Task09.java
@@ -0,0 +1,13 @@
+package by.epam.training.module02.multiarray.task09;
+
+// Задана матрица неотрицательных чисел.
+// Посчитать сумму элементов в каждом столбце.
+// Определить, какой столбец содержит максимальную сумму.
+
+public class Module02Task09 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}

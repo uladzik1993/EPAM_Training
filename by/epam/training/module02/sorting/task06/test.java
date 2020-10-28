38d3734fcc528c24d5fc389f095cf72e2accec4a
Vladislav Rubanov <uladzik@mail.ru>
2020-10-29 00:41:57 +0300

    add new task in multiarrays

--- by/epam/training/module02/sorting/task06/test.java
+++
@@ -1,8 +0,0 @@
-package by.epam.training.module02.sorting.task06;
-
-public class test {
-    public static void main(String[] args) {
-        int a = (int) (Math.random() * 100);
-        System.out.println(a);
-    }
-}

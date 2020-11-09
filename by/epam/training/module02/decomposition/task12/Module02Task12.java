b45a3a399e13dd4307dc3bc3917ce6b9f08df327
Vladislav Rubanov <uladzik@mail.ru>
2020-11-10 00:37:00 +0300

    add new task in decomposition

--- by/epam/training/module02/decomposition/task12/Module02Task12.java
+++ by/epam/training/module02/decomposition/task12/Module02Task12.java
@@ -3,6 +3,7 @@ package by.epam.training.module02.decomposition.task12;
 // Даны натуральные числа К и N. Написать метод(методы) формирования массива А,
 // элементами которого являются числа, сумма цифр которых равна К и которые не большее N.
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Module02Task12 {
@@ -11,12 +12,21 @@ public class Module02Task12 {
         int n = 5;
         int l = 5;
         int[] a = new int[l];
+
+        buidArray(k, n, a);
     }
 
-public static void buidArray (int k, int n, int l,int[] array) {
-    Random rand = new Random();
-    for (int i = 0; i < array.length; i++) {
+    public static void buidArray(int k, int n, int[] array) {
+        int sum = 0;
+        while (sum != k) {
+            sum = 0;
+            Random rand = new Random();
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (Math.random() * (n + 1));
+                sum += array[i];
+            }
+        }
+        System.out.println(Arrays.toString(array));
 
     }
-}
 }

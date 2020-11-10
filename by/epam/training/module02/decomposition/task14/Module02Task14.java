2909b672587ceaadec252c7110b48bb3a7864646
Vladislav Rubanov <uladzik@mail.ru>
2020-11-11 00:33:00 +0300

    add new task in decomposition

--- by/epam/training/module02/decomposition/task14/Module02Task14.java
+++ by/epam/training/module02/decomposition/task14/Module02Task14.java
@@ -4,5 +4,11 @@ package by.epam.training.module02.decomposition.task14;
 // если сумма его цифр, возведенная в степень n, равна самому числу.
 // Найти все числа Армстронга от 1 до k. Для решения задачи использовать декомпозицию.
 
+import java.util.Arrays;
+
 public class Module02Task14 {
+    public static void main(String[] args) {
+        int k = 2000;
+
+    }
 }

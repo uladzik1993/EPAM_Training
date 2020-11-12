2e1e8f9ce507f60a26532ed545a6f2752a7b3a3d
Vladislav Rubanov <uladzik@mail.ru>
2020-11-12 23:46:54 +0300

    add new task in decomposition

--- by/epam/training/module02/decomposition/task14/Module02Task14.java
+++ by/epam/training/module02/decomposition/task14/Module02Task14.java
@@ -4,37 +4,39 @@ package by.epam.training.module02.decomposition.task14;
 // если сумма его цифр, возведенная в степень n, равна самому числу.
 // Найти все числа Армстронга от 1 до k. Для решения задачи использовать декомпозицию.
 
-import java.util.Arrays;
-
 public class Module02Task14 {
     public static void main(String[] args) {
         int k = 1000;
 
-        checkIfAmstrong(k);
-
+        processNumbers(k);
     }
 
-    private static void checkIfAmstrong(int userNumber) {
-        int remainder = 0;
-        int originalNumber = userNumber;
+    private static void processNumbers(int userNumber) {
         for (int i = 1; i <= userNumber; i++) {
-            int rate = 0;
-            originalNumber = i;
-            while (originalNumber != 0) {
-                originalNumber /= 10;
-                rate++;
-            }
-
-            int number = i;
-            int result = 0;
-
-            while (number != 0) {
-                remainder = number % 10;
-                result += Math.pow(remainder, rate);
-                number /= 10;
-            }
-            if (result == i)
+            if (checkIfArmstrong(i)) {
                 System.out.println("Найдено число Амстронга: " + i);
+            }
         }
+    }
+
+    private static boolean checkIfArmstrong(int i) {
+        int originalNumber = i;
+        int remainder;
+        int rate = 0;
+
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            rate++;
+        }
+
+        int number = i;
+        int result = 0;
+
+        while (number != 0) {
+            remainder = number % 10;
+            result += Math.pow(remainder, rate);
+            number /= 10;
+        }
+        return result == i;
     }
 }
--- by/epam/training/module02/decomposition/task15/Module02Task15.java
+++ by/epam/training/module02/decomposition/task15/Module02Task15.java
@@ -3,56 +3,28 @@ package by.epam.training.module02.decomposition.task15;
 // Найти все натуральные n-значные числа, цифры в которых образуют строго возрастающую последовательность
 // (например, 1234, 5789). Для решения задачи использовать декомпозицию.
 
-import by.epam.training.module02.sorting.task06.Module02Task06;
-
-import java.util.Arrays;
-
 public class Module02Task15 {
+
     public static void main(String[] args) {
-        int number = 130;
+        int maxNumber = 1000;
 
-
-        int part = 0;
-        for (int i = 1; i <= number; i++) {
-            int count = 0;
-            int tmp = i;
-            while (tmp != 0) {
-                tmp /= 10;
-                count++;
-            }
-            tmp = i;
-            int[] mass = new int[count];
-            int cell = 0;
-            while (tmp != 0) {
-                part = tmp % 10;
-                tmp /= 10;
-                mass[cell] = part;
-                cell++;
-            }
-            if (count > 1) {
-                Module02Task06.shellSort(mass);
-                boolean flag = almostIncreasingSequence(mass);
-                if (flag) {
-                    System.out.println(Arrays.toString(mass));
-                }
-            }
-        }
+        findStrictlyIncreasingNum(0, " ", 4);
     }
 
-    private static boolean almostIncreasingSequence(int[] sequence) {
-        int max = Integer.MIN_VALUE, count = 0;
-        boolean flag = true;
-        for (int j = 0; j < sequence.length; j++) {
-            if (max >= sequence[j]) {
-                count++;
-            } else
-                max = sequence[j];
-            if (count > 1) {
-                flag = false;
-                break;
-            }
+    private static void findStrictlyIncreasingNum(int start, String out, int n)
+    {
+        if (n == 0)
+        {
+            System.out.println(out + " ");
+            return;
         }
-        return flag;
+
+        for (int i = start; i <= 9; i++)
+        {
+            String str = out + Integer.toString(i);
+
+            findStrictlyIncreasingNum(i + 1, str, n - 1);
+        }
     }
 }
 

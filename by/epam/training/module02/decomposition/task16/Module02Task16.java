c0484d5b039e10fab0ff83bafc3a27d2f9b7abd2
Vladislav Rubanov <uladzik@mail.ru>
2020-11-14 14:58:34 +0300

    add new task in decomposition

--- by/epam/training/module02/decomposition/task16/Module02Task16.java
+++ by/epam/training/module02/decomposition/task16/Module02Task16.java
@@ -3,5 +3,78 @@ package by.epam.training.module02.decomposition.task16;
 // Написать программу, определяющую сумму n - значных чисел, содержащих только нечетные цифры.
 // Определить также, сколько четных цифр в найденной сумме. Для решения задачи использовать декомпозицию.
 
+import java.util.Scanner;
+
 public class Module02Task16 {
+    public static void main(String[] args) {
+        System.out.println("Начнем поиск чисел содержащих только не четные числа!");
+        int limit = enter("Введите ограничение для поиска:");
+        int sum = 0;
+        int evenNumber = 0;
+        System.out.println("Сумма чисел содержащих только нечетные цифры равна: " + sumOfUnevenNumbers(limit));
+        System.out.println("Колличество четных цифр в найденной сумме: " + countEven(sumOfUnevenNumbers(limit)));
+    }
+
+    public static void sumOfUneven(int userLimit) {
+
+    }
+
+    static boolean checkUneven(int number) {
+        int part = 0;
+        boolean result = false;
+        while (number != 0) {
+            part = number % 10;
+            number /= 10;
+            if (uneven(part)) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    static int countEven(int number) {
+        int part = 0;
+        int count = 0;
+        while (number != 0) {
+            part = number % 10;
+            number /= 10;
+            if (part > 0) {
+                if (even(part)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    static boolean uneven(int a) {
+        return (a % 2 != 0);
+    }
+
+    static boolean even(int a) {
+        return (a % 2 == 0);
+    }
+
+    static int sumOfUnevenNumbers(int userLimit) {
+        int sum = 0;
+        for (int num = 1; num <= userLimit; num++) {
+            if (checkUneven(num)) {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+    public static int enter(String massage) {
+        System.out.println(massage);
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.println("Введенные данные не являются натуральным числом. Повторите ввод:");
+        }
+        return scanner.nextInt();
+    }
 }
---
+++ by/epam/training/module03/task01/Module03Task01.java
@@ -0,0 +1,6 @@
+package by.epam.training.module03.task01;
+
+// Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.
+
+public class Module03Task01 {
+}

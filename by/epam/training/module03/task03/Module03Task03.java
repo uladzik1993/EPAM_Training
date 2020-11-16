1738e1ffe66e3f2087cf05da238466f8511508a6
Vladislav Rubanov <uladzik@mail.ru>
2020-11-16 23:55:16 +0300

    add new task in string

---
+++ by/epam/training/module03/task03/Module03Task03.java
@@ -0,0 +1,21 @@
+package by.epam.training.module03.task03;
+
+// В строке найти количество цифр.
+
+public class Module03Task03 {
+
+    public static void main(String[] args) {
+        String message = "At 5 o'clock I want to borrow 2 books from the 7th library.";
+        System.out.println(howManyDigits(message));
+    }
+
+    public static int howManyDigits(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

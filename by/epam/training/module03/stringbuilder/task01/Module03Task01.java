2d8311c4706c615b164aa60b01fcd16edde1ecc9
Vladislav Rubanov <uladzik@mail.ru>
2020-11-20 01:08:32 +0300

    add new task in stringBuilder

--- by/epam/training/module03/stringbuilder/task01/Module03Task01.java
+++ by/epam/training/module03/stringbuilder/task01/Module03Task01.java
@@ -1,4 +1,31 @@
 package by.epam.training.module03.stringbuilder.task01;
 
+// Дан текст (строка). Найдите наибольшее количество подряд идущих пробелов в нем.
+
 public class Module03Task01 {
+
+    public static void main(String[] args) {
+        String text = "Меня   зовут Ник,    мой лучший     друг               Майк.";
+
+        System.out.println(text);
+        System.out.println("Наибольшее колличество подряд идущих пробелов: " + countingGaps(text));
+    }
+
+    private static int countingGaps(String str) {
+        String tmp = " ";
+        int countMax = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                int count = 0;
+                while (str.charAt(i++) == ' ') {
+                    count++;
+                    if (count > countMax) {
+                        countMax = count;
+                    }
+                }
+            }
+        }
+        return countMax;
+    }
 }
--- by/epam/training/module03/stringbuilder/task02/Module03Task02.java
+++ by/epam/training/module03/stringbuilder/task02/Module03Task02.java
@@ -1,4 +1,25 @@
 package by.epam.training.module03.stringbuilder.task02;
 
+// В строке вставить после каждого символа 'a' символ 'b'.
+
 public class Module03Task02 {
+
+    public static void main(String[] args) {
+        String text = "Eat some more of these soft French buns and have some tea";
+
+        System.out.println("Ваш текст: " + text);
+        System.out.println();
+        System.out.println("Ваш текст после исправления: " + fixThis(text));
+    }
+
+    private static String fixThis(String str) {
+        int tmp = 0;
+        String tmpStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a') {
+                tmpStr += str.charAt(i) + "b";
+            } else tmpStr += str.charAt(i);
+        }
+        return tmpStr;
+    }
 }

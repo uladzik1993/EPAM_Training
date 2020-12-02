a50740911ef4b51ce22092269060c5e36b26e978
Vladislav Rubanov <uladzik@mail.ru>
2020-12-02 21:12:42 +0300

    add new task in stringBuilder

---
+++ by/epam/training/module03/stringbuilder/task08/Module03Task08.java
@@ -0,0 +1,54 @@
+package by.epam.training.module03.stringbuilder.task08;
+
+// Вводится строка слов, разделенных пробелами.
+// Найти самое длинное слово и вывести его на экран.
+// Случай, когда самых длинных слов может быть несколько, не обрабатывать.
+
+import java.util.Scanner;
+
+public class Module03Task08 {
+    public static void main(String[] args) {
+        String words = enter("Введите несколько слов через пробел, а я выберу самое длиинное");
+        viewLongerWord(words);
+    }
+
+    private static void viewLongerWord(String str) {
+        String[] strArray = creatArray(str);
+        str = searchLong(strArray);
+        System.out.println(str);
+    }
+
+    private static String enter(String massage) {
+        System.out.println(massage);
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        if (scanner.hasNext()) {
+            sb.append(scanner.nextLine());
+        }
+        return massage = sb.toString();
+    }
+
+    private static String[] creatArray(String str) {
+        String[] peopleArray = str.split(" ");
+        return peopleArray;
+    }
+
+    private static String searchLong(String[] arr) {
+        String longer = null;
+        int size = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length() >= size) {
+                if (arr[i].length() == size) {
+                    count += 1;
+                }
+                longer = arr[i];
+                size = arr[i].length();
+            }
+        }
+        if (count > 0) {
+            longer = "Два и более длинных слова";
+        }
+        return longer;
+    }
+}
